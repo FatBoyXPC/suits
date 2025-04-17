63bc27ed4793858943ec9e3721b8e6a86ee028df
James LaChance <fatboyxpc@gmail.com>
2025-04-17 04:23:18 -0400

    Trying to get specter going...

---
+++ nixos-modules/single-ext4.nix
@@ -0,0 +1,35 @@
+# https://github.com/nix-community/disko/blob/76c0a6dba345490508f36c1aa3c7ba5b6b460989/example/simple-efi.nix
+# don't forget to set disko.devices.disk.main.device
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
