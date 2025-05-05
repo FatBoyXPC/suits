9d9441f4b12743b49493cec9f3ea1f3f9fefaeb4
James LaChance <fatboyxpc@gmail.com>
2025-05-04 22:37:54 -0400

    Much nicer cursor!

--- hosts/specter/home.nix
+++ hosts/specter/home.nix
@@ -6,6 +6,15 @@
       { source = ../../dotfiles/${source}; }
     ) (import ../../dotfiles/links.nix);
 
+  home.pointerCursor = {
+    x11.enable = true;
+    gtk.enable = true;
+
+    package = pkgs.quintom-cursor-theme;
+    name = "Quintom_Ink";
+    size = 45;
+  };
+
   dconf.settings."org/gnome/desktop/interface"."gtk-key-theme" = "Emacs";
 
   home.stateVersion = "25.05";
