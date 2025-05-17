12cfef725359ec6d2bd3f981cdb31919d0a093e8
James LaChance <fatboyxpc@gmail.com>
2025-05-17 00:14:53 -0400

    A bit more cleanup!

--- flake-modules/nixos-hosts.nix
+++ flake-modules/nixos-hosts.nix
@@ -12,7 +12,7 @@ let
   evalConfig =
     { hostname }:
     inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"; # <<< TODO: live in host rather than be hardcoded
+      system = null;
       modules = [
         inputs.disko.nixosModules.disko # <<< TODO: hosts should be able to import things they need, such as disko
         (hostsDir + "/${hostname}/configuration.nix")
