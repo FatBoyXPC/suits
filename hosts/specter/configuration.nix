cb9f81105de7567f9aa1448d107e6a46470fcb49
James LaChance <fatboyxpc@gmail.com>
2025-07-30 17:56:19 -0400

    update flake

--- hosts/specter/configuration.nix
+++ hosts/specter/configuration.nix
@@ -46,8 +46,8 @@
   time.timeZone = "America/New_York";
 
   # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # networking.proxy.default = "http://user:password@proxy:port/";
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
