305f479f2f8fe9bdb2a5c0f4faa26f9ef563f09b
James LaChance <fatboyxpc@gmail.com>
2026-02-15 06:07:17 -0500

    Minimal transmission settings for @jfly (see full commit msg)
    It might be worth pointing out that you can either -enabled = false or
    you can have the whitelists set to "*".
    
    Example:
    rpc-whitelist = "*" is synonymous with rpc-whitelist-enabled = "false"

---
+++ hosts/paulson/arr/transmission.nix
@@ -0,0 +1,43 @@
+{ config, ... }:
+let
+  host = config.vpnNamespaces.wg.namespaceAddress;
+  port = config.services.transmission.settings.rpc-port;
+  mediaDir = "/mnt/cosmos/media";
+in
+{
+  services = {
+    transmission = {
+      enable = true;
+      group = "media";
+      settings = {
+        download-dir = "${mediaDir}/torrents";
+        incomplete-dir = "${config.services.transmission.settings.download-dir}/incomplete";
+        ratio-limit-enabled = true;
+        rpc-bind-address = "0.0.0.0";
+        rpc-whitelist-enabled = "false";
+        rpc-host-whitelist-enabled = "false";
+      };
+    };
+
+    nginx.virtualHosts."transmission.fatboyxpc.com" = {
+      locations."/" = {
+        proxyPass = "http://${host}:${toString port}";
+      };
+    };
+  };
+
+  systemd.services.transmission = {
+    vpnConfinement = {
+      enable = true;
+      vpnNamespace = "wg";
+    };
+  };
+
+  vpnNamespaces.wg.portMappings = [
+    {
+      from = port;
+      to = port;
+      protocol = "tcp";
+    }
+  ];
+}
