07207006e22837c2ac6d34c9d442152f02115039
James LaChance <fatboyxpc@gmail.com>
2026-05-14 09:55:25 -0400

    Inline so we can only have nix files in here

--- packages/my-nix/bin/cgl.nix
+++ packages/my-nix/bin/cgl.nix
@@ -12,6 +12,18 @@ pkgs.writeShellApplication {
   ];
 
   text = ''
-    python3 ${./git-commit-link.py} "$1" | clipit
+    python3 - "$1" <<'PYTHON' | clipit
+    # Git Commit Link
+    # That pun on git/get! LOLOL!
+
+    from subprocess import check_output
+    import sys
+
+    commit = sys.argv[1]
+    remoteOrigin = check_output('git config --get remote.origin.url', shell=True, universal_newlines=True).rstrip()
+    url = remoteOrigin.replace(':', '/').replace('git@', 'https://').replace('.git', '/')
+    url = url + 'commit/' + commit
+    print(url)
+    PYTHON
   '';
 }
