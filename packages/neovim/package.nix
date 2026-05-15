7d72b015bb9da1a7de2d7e07411fc04857b000d6
James LaChance <fatboyxpc@gmail.com>
2026-05-15 02:02:32 -0400

    Let's respect my unfree packages mmmkay!

--- packages/neovim/package.nix
+++ packages/neovim/package.nix
@@ -6,62 +6,65 @@
   ...
 }:
 
-inputs.nixvim.legacyPackages.${system}.makeNixvim {
-  extraConfigVim = ''
-    let g:clipboard='osc52'
-    let g:configPath=$HOME.'/.vim'
+inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule {
+  inherit pkgs;
+  module = {
+    extraConfigVim = ''
+      let g:clipboard='osc52'
+      let g:configPath=$HOME.'/.vim'
 
-    ${builtins.readFile ./functions.vim}
-    ${builtins.readFile ./mappings.vim}
-    ${builtins.readFile ./myrc.vim}
-    ${builtins.readFile ./plugins.vim}
-  '';
+      ${builtins.readFile ./functions.vim}
+      ${builtins.readFile ./mappings.vim}
+      ${builtins.readFile ./myrc.vim}
+      ${builtins.readFile ./plugins.vim}
+    '';
 
-  extraConfigLuaPost = builtins.readFile ./nvim-specific.lua;
+    extraConfigLuaPost = builtins.readFile ./nvim-specific.lua;
 
-  plugins =
-    builtins.listToAttrs (
-      map (x: (lib.nameValuePair x { enable = true; })) [
-        "bufdelete"
-        "cmp"
-        "cmp-buffer"
-        "cmp-cmdline"
-        "cmp-nvim-lsp"
-        "cmp-nvim-lsp-signature-help"
-        "cmp-path"
-        "fugitive"
-        "gitgutter"
-        "indent-blankline"
-        "lightline"
-        "lsp"
-        "lspconfig"
-        "markdown-preview"
-        "nvim-autopairs"
-        "sandwich"
-        "tagbar"
-        "telescope"
-        "undotree"
-      ]
-    )
-    // {
-      web-devicons.enable = false;
-      lsp.servers = {
-        phpactor.enable = true;
+    plugins =
+      builtins.listToAttrs (
+        map (x: (lib.nameValuePair x { enable = true; })) [
+          "bufdelete"
+          "cmp"
+          "cmp-buffer"
+          "cmp-cmdline"
+          "cmp-nvim-lsp"
+          "cmp-nvim-lsp-signature-help"
+          "cmp-path"
+          "fugitive"
+          "gitgutter"
+          "indent-blankline"
+          "lightline"
+          "lsp"
+          "lspconfig"
+          "markdown-preview"
+          "nvim-autopairs"
+          "sandwich"
+          "tagbar"
+          "telescope"
+          "undotree"
+        ]
+      )
+      // {
+        web-devicons.enable = false;
+        lsp.servers = {
+          phpactor.enable = true;
+        };
       };
-    };
 
-  extraPlugins = with pkgs.vimPlugins; [
-    lightline-bufferline
-    nerdcommenter
-    plenary-nvim
-    telescope-fzf-native-nvim
-    telescope-live-grep-args-nvim
-    telescope-ui-select-nvim
-    ultisnips
-    vim-dim
-    vim-gista
-    vim-polyglot
-    vim-rhubarb
-    vim-test
-  ];
+    extraPlugins = with pkgs.vimPlugins; [
+      lightline-bufferline
+      nerdcommenter
+      plenary-nvim
+      telescope-fzf-native-nvim
+      telescope-live-grep-args-nvim
+      telescope-ui-select-nvim
+      ultisnips
+      vim-dim
+      vim-gista
+      vim-polyglot
+      vim-rhubarb
+      vim-test
+    ];
+  };
 }
