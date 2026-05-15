681bde9c7f9f34c7c4167df2ade2af43af82aeca
James LaChance <fatboyxpc@gmail.com>
2026-05-15 01:45:44 -0400

    Clipboard is finicky, but satty ftw!
    Satty has this copy-command option that makes it so we can hang onto the
    image in the clipboard after satty exits. That's nice!

--- packages/screenshot/package.nix
+++ packages/screenshot/package.nix
@@ -5,10 +5,11 @@ pkgs.writeShellApplication {
 
   runtimeInputs = with pkgs; [
     shotgun
+    xclip
     satty
   ];
 
   text = ''
-    shotgun /dev/stdout | satty -f - --initial-tool crop --early-exit --actions-on-enter save-to-file
+    shotgun /dev/stdout | satty -f - --initial-tool crop --early-exit --actions-on-enter save-to-file --copy-command "xclip -selection clipboard -target image/png -in"
   '';
 }
