50d19b910c7dbdc1c0498cd9f394630cc6bf3e91
James LaChance <fatboyxpc@gmail.com>
2026-05-29 17:02:19 -0400

    Nice to have zsh-artisan back!

--- hosts/specter/configuration.nix
+++ hosts/specter/configuration.nix
@@ -30,6 +30,7 @@
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.users.james = ./home.nix;
+      home-manager.extraSpecialArgs = { inherit self'; };
     }
   ];
 
--- hosts/specter/omz.nix
+++ hosts/specter/omz.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, self', ... }:
 
 {
   home.file.".oh-my-zsh".source =
@@ -12,6 +12,7 @@
         postBuild = ''
           ln -s ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k $out/share/oh-my-zsh/custom/themes/powerlevel10k
           ln -s ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting $out/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+          ln -s ${self'.packages.zsh-artisan}/share/zsh-artisan $out/share/oh-my-zsh/custom/plugins/artisan
         '';
       };
 
