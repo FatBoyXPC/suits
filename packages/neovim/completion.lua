80e68f0fec751e6cc525d4673ef60965004b1b00
James LaChance <fatboyxpc@gmail.com>
2026-06-06 22:42:45 -0400

    Codex suggestion
    Codex said that I have enabled the cmdline completion but haven't
    actually configured to do anything. I'm not entirely convinced having
    command line completion is going to be helpful, but it at least does
    show completion suggestions for parameters after you type a command
    name, which does seem like it could be helpful. I'll try it out.
    
    I can also do completion suggestions for searches (with /), however, I
    really don't think for my workflow that's useful. I tend to know what I
    want to search when I start a search.
    
    Regardless, we can add this snippet if I decide I want it:
    cmp.setup.cmdline('/', {
        mapping = cmp.mapping.preset.cmdline(),
        sources = {
            { name = 'buffer' },
        },
    })

--- packages/neovim/completion.lua
+++ packages/neovim/completion.lua
@@ -47,3 +47,11 @@ cmp.setup {
       },
     }
 }
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'path' },
+        { name = 'cmdline' },
+    },
+})
